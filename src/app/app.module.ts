f0468dc988ba689708ab41e919eb955686b0b3c1
Daniel Beß <daniel.bess@gmx.de>
2020-08-22 01:49:17 +0200

    Bootstrap angebunden und rudimentaeres Impressum hinzugefuegt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MainStatDisplayComponent } from './main-stat-display/main-stat-display.component';
 import { SubStatDisplayComponent } from './sub-stat-display/sub-stat-display.component';
+import { ImpressumComponent } from './impressum/impressum.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainStatDisplayComponent,
-    SubStatDisplayComponent
+    SubStatDisplayComponent,
+    ImpressumComponent
   ],
   imports: [
     BrowserModule
